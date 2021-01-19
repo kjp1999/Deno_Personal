ecdf48ae5b3701fe492bd718fd40f4ebd3a5c09c
kjp1999 <kjp199911@gmail.com>
2021-01-19 12:59:02 -0700

    Comments

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
+//imports for web framework and etc
 import { opine, serveStatic } from "https://deno.land/x/opine@1.0.2/mod.ts";
 import { renderFileToString } from "https://deno.land/x/dejs@0.9.3/mod.ts";
 import { join, dirname } from "https://deno.land/x/opine@1.0.2/deps.ts";
+//instantiate opine
 const app = opine();
 
 const __dirname = dirname(import.meta.url);
@@ -14,8 +16,6 @@ app.set('view engine', 'html');
 app.get('/', (req, res) => {
   res.render('index', { title: 'Kaleb Park' });
 });
-
-
 
 app.listen(3000);
 console.log('running on port 3000');
