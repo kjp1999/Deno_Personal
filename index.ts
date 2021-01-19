8a71aca800cdfd86668b8f3a1060722b55e1c6db
kjp1999 <kjp199911@gmail.com>
2021-01-18 17:05:45 -0700

    Personal Website
    Personal Website written in Deno. First Commit on this project.

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { opine, serveStatic } from "https://deno.land/x/opine@1.0.2/mod.ts";
+import { renderFileToString } from "https://deno.land/x/dejs@0.9.3/mod.ts";
+import { join, dirname } from "https://deno.land/x/opine@1.0.2/deps.ts";
+const app = opine();
+
+const __dirname = dirname(import.meta.url);
+
+app.engine('.html', renderFileToString);
+app.use(serveStatic(join(__dirname, 'public')));
+app.use(serveStatic( "public"));
+
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Kaleb Park' });
+});
+
+
+
+app.listen(3000);
+console.log('running on port 3000');
---
+++ public/js/myscripts.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $(document).ready(function (){
+        $('.main-carousel').flickity({
+            // options
+            //wrapAround: true,
+            groupCells: true,
+            wrapAround: true
+
+        });
+    });
+  })(jQuery)
